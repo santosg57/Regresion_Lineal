edbc7f621cdf8c79f735e94691bf274dda3bc35a
santosg57 <lgs@unam.mx>
2017-11-16 05:13:47 -0600

    Update p2.R

--- p2.R
+++ p2.R
@@ -1,1 +1,15 @@
-s
+xx <- seq(0,25, 1)
+yy <- .1*((xx/10-1)^2 -1)
+
+n = length(xx)
+x = sample(xx,n, replace=T)
+y = sample(yy,n, replace=T)
+
+plot(x,y)
+
+linea = lm(y ~ x)
+print(summary(linea))
+abline(linea)
+
+print(cor.test(x,y, method='spearman'))
+
