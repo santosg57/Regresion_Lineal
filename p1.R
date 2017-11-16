3b8b624b6197dd7cca41a46fe6515b6221a7a764
santosg57 <lgs@unam.mx>
2017-11-16 05:12:21 -0600

    Update p1.R

--- p1.R
+++ p1.R
@@ -1,1 +1,28 @@
-hh
+xx = seq(0,4,.4)
+yy = 2-xx^2 #xx^4 + 10
+
+n = length(xx)
+x = xx #sample(xx, n, replace=T)
+y1 = yy #sample(yy, n, replace=T)
+
+plot(x,y1)
+
+linea = lm(y1 ~  x)
+
+print(summary(linea))
+
+abline(linea)
+
+print(cor.test(x,y1, method='spearman'))
+
+yy = 2 + x^2 #xx^4 - 10
+x = xx #sample(xx, n, replace=T)
+y2 = yy #sample(yy, n, replace=T)
+
+plot(x,y2)
+linea2 = lm(y2 ~ x)
+print(summary(linea2))
+abline(linea2)
+
+print(cor.test(x,y2, method='spearman'))
+
